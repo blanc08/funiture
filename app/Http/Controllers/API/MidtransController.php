e5a3723e8e98f60a75fd6e4fa63bd317a06c25ef
bugss02 <bagusoktaviadi1@gmail.com>
2021-09-05 11:13:23 +0700

    Midtrans Callback Done

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Midtrans\Config;
+use Midtrans\Notification;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+
+class MidtransController extends Controller
+{
+    public function callback()
+    {
+        // Set Konfigurasi midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        // Buat instance midtrans notification
+        $notification = new Notification();
+
+        // Assign ke variable untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+        // Get transaction id
+        $order = explode('-', $order_id);
+
+        // Mencari transaksi berdasarkan id
+        $transaction = Transaction::findOrFail($order[1]);
+
+        // Handle notification status midtrans
+        if ($status == 'capture') {
+            if ($type == 'credit_card') {
+                if ($fraud == 'challenge') {
+                    $transaction->status = 'PENDING';
+                } else {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        } elseif ($status == 'settlement') {
+            $transaction->status = 'SUCCESS';
+        } elseif ($status == 'pending') {
+            $transaction->status = 'PENDING';
+        } elseif ($status == 'deny') {
+            $transaction->status = 'PENDING';
+        } elseif ($status == 'expire') {
+            $transaction->status = 'CANCELLED';
+        } elseif ($status == 'cancell') {
+            $transaction->status = 'CANCELLED';
+        }
+
+        // Simpan transaksi
+        $transaction->save();
+
+        // Return respone untuk midtrans
+        return response()->json([
+            'meta' => [
+                'code' => 200,
+                'message' => 'Midtrans Notification Success',
+            ],
+        ]);
+    }
+}
